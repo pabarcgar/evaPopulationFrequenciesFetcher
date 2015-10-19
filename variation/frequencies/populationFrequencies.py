f71b7444f9ac45030a2477b9f550f3aa4759955f
Pablo Arce <pablo.arce.garcia@gmail.com>
2015-10-19 16:46:09 +0100

    fixed minor bug

--- variation/frequencies/populationFrequencies.py
+++ variation/frequencies/populationFrequencies.py
@@ -13,4 +13,4 @@ class PopulationFrequencies:
 
     @staticmethod
     def format_frequency(frequency):
-        return '{:.5f}'.format(frequency).rstrip('0')
+        return '{:.5f}'.format(frequency).rstrip('0').rstrip('.')
